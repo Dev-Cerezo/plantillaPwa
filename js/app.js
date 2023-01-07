570e04533709ce5a2956e7fd8c9fe93f0872adbe
Dev-Cerezo <dev.ti@grupoelcerezo.com>
2023-01-07 11:38:32 -0600

    on off line

--- js/app.js
+++ js/app.js
@@ -11,3 +11,27 @@ if (url.includes('localhost')) {
 
     navigator.serviceWorker.register(swLocation)
 }
+
+
+function onFunction() {
+
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'ya esta en linea',
+      showConfirmButton: true,
+      timer: 10000
+    })
+  }
+  
+  function offFunction() {
+    
+
+    Swal.fire({
+      position: 'top-end',
+      icon: 'error',
+      title: 'ya no esta en linea',
+      showConfirmButton: true,
+      timer: 10000
+    })
+  }
